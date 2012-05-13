f3be4c5e75361e59ba080d843ad30db9642031a0
Achilleas Tsoumitas <knorcedger@gmail.com>
2012-05-13 11:04:58 +0300

    Added a date formatting function and localization

--- ODate.js
+++ ODate.js
@@ -1,15 +1,26 @@
 /*jshint smarttabs: true*/
 
+var ODate;
+
 (function() {
 	"use strict";
 
 	/**
 	 * An object that provides usefull functions with dates
 	 */
-	var ODate = function() {
-			this._todayString = this._findTodayDate();
-			this._todayObject = new Date();
-		};
+	ODate = function() {
+		this._todayString = this._findTodayDate();
+		this._todayObject = new Date();
+
+		this.locale = "en";
+		this.regional = {};
+		this.regional["en"] = {};
+		this.regional["en"].dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		this.regional["en"].dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		//this.regional["en"].dayNamesMin = ["Su","Mo","Tu","We","Th","Fr","Sa"];
+		this.regional["en"].monthNamesShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		this.regional["en"].monthNames = ["January", "February", "March", "April", "May", "June", "July", "AUgust", "September", "October", "November", "December"];
+	};
 
 	/**
 	 * Updates the todayDate used
@@ -23,33 +34,11 @@
 	};
 
 	/**
-	 * Create a date object from the given date string
-	 * @return {Object} A date object
-	 */
-	ODate.prototype._updateTodayObject = function(newDate) {
-		var dateArray = newDate.split("/");
-		var todayObject = new Date(dateArray[2], dateArray[1], dateArray[0]);
-
-		this._todayObject = todayObject;
-	};
-
-	/**
 	 * Returns the today date as it is currenctly saved in the object
 	 * @return {string}
 	 */
 	ODate.prototype.today = function() {
 		return this._todayString;
-	};
-
-	/**
-	 * Returns the today date in this format: "08/05/2012"
-	 * @return {String}
-	 */
-	ODate.prototype._findTodayDate = function() {
-		var today = new Date();
-		var todayDate = this._addZero(today.getDate()) + '/' + this._addZero(today.getMonth() + 1) + '/' + today.getFullYear();
-
-		return todayDate;
 	};
 
 	/**
@@ -91,6 +80,63 @@
 	};
 
 	/**
+	 * Returns a formatted date given a date object and the desired format
+	 * @param  {Date} dateObject A date object
+	 * @param  {String} format     The format we want. Check the supported expressions below
+	 * @return {String}            The formatted date
+	 */
+	ODate.prototype.formatDate = function(dateObject, format) {
+
+		var expressions = ["dddd", "ddd", "dd", "d", "mmmm", "mmm", "mm", "m", "yyyy"];
+		var replacements = [
+				this.regional[this.locale].dayNames[dateObject.getDay()], 
+				this.regional[this.locale].dayNamesShort[dateObject.getDay()], 
+				this._addZero(dateObject.getDate()), 
+				dateObject.getDate(), 
+				this.regional[this.locale].monthNames[dateObject.getMonth()], 
+				this.regional[this.locale].monthNamesShort[dateObject.getMonth()],
+				this._addZero(dateObject.getMonth()), 
+				dateObject.getMonth(), 
+				dateObject.getFullYear()
+			];
+
+		var result = format;
+
+		for (var i = 0, length = expressions.length; i < length; i++) {
+			var regEx = new RegExp(expressions[i]);
+			if (regEx.test(format)) {
+				result = result.replace(expressions[i], replacements[i]);
+				format = format.replace(expressions[i], "");
+			}
+
+		}
+
+		return result;
+	};
+
+	/**
+	 * Create a date object from the given date string
+	 * @return {Object} A date object
+	 */
+	ODate.prototype._updateTodayObject = function(newDate) {
+		var dateArray = newDate.split("/");
+		var todayObject = new Date(dateArray[2], dateArray[1], dateArray[0]);
+
+		this._todayObject = todayObject;
+	};
+
+	/**
+	 * Returns the today date in this format: "08/05/2012"
+	 * @return {String}
+	 */
+	ODate.prototype._findTodayDate = function() {
+		var today = new Date();
+		var todayDate = this._addZero(today.getDate()) + "/" + this._addZero(today.getMonth() + 1) + "/" + today.getFullYear();
+
+		return todayDate;
+	};
+
+	/**
 	 * Add a leeding zeros to numbers smaller than 10
 	 * @param  {String} value
 	 * @return {String}
@@ -98,7 +144,7 @@
 	ODate.prototype._addZero = function(value) {
 		var result;
 		if (value < 10) {
-			result = '0' + value;
+			result = "0" + value;
 		} else {
 			result = value;
 		}
@@ -106,3 +152,8 @@
 		return result;
 	};
 }());
+
+
+var d = new ODate();
+console.log(d.formatDate(new Date(), "dddd/mm/yyyy"));
+//d.setLocalization.monthNames("");
