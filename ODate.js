27d507df5c0823915572bfadfb537407f6128c94
Achilleas Tsoumitas <knorcedger@gmail.com>
2012-06-07 14:53:42 +0300

    Changed the code formats supported. Full documentation will be released soon

--- ODate.js
+++ ODate.js
@@ -1,29 +1,39 @@
 /*jshint smarttabs: true*/
 
+/**
+ * A date library providing useful date functions around a today date.
+ * 
+ * @author Achilleas Tsoumitas (https://twitter.com/#!/knorcedger)
+ * @version 1.0.0
+ * @source https://github.com/Knorcedger/ODate.js
+ * 
+ * @licence http://creativecommons.org/licenses/by/3.0/
+ */
+
 var ODate;
 
 (function() {
 	"use strict";
 
 	/**
-	 * An object that provides usefull functions with dates
+	 * The constructor that also defines some default values
 	 */
-	ODate = function() {
-		// set defaults
-		this._todayObject = new Date();
-		this._dateObject = new Date();
+	ODate = function() {};
 
-		this.dateFormat = "dd/mm/yyyy";
+	/**
+	 * Set defaults
+	 */
+	ODate.prototype._dateObject = new Date();
 
-		this.locale = "en";
-		this.regional = {};
-		this.regional.en = {};
-		this.regional.en.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-		this.regional.en.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-		//this.regional.en.dayNamesMin = ["Su","Mo","Tu","We","Th","Fr","Sa"];
-		this.regional.en.monthNamesShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-		this.regional.en.monthNames = ["January", "February", "March", "April", "May", "June", "July", "AUgust", "September", "October", "November", "December"];
-	};
+	ODate.prototype.dateFormat = "dd/mm/yyyy";
+
+	ODate.prototype.locale = "en";
+	ODate.prototype.regional = {};
+	ODate.prototype.regional.en = {};
+	ODate.prototype.regional.en.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	ODate.prototype.regional.en.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	ODate.prototype.regional.en.monthNamesShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	ODate.prototype.regional.en.monthNames = ["January", "February", "March", "April", "May", "June", "July", "AUgust", "September", "October", "November", "December"];
 
 	/**
 	 * Updates the todayDate object
@@ -32,7 +42,7 @@ var ODate;
 	 * @param {Number} day
 	 */
 	ODate.prototype.setToday = function(year, month, day) {
-		this._todayObject = new Date(year, month, day);
+		this._dateObject = new Date(year, month, day);
 	};
 
 	/**
@@ -46,41 +56,7 @@ var ODate;
 		var monthsToAdd = months && parseInt(months, 10) || 0;
 		var daysToAdd = days && parseInt(days, 10) || 0;
 		
-		this._dateObject = new Date(this._todayObject.getFullYear() + yearsToAdd, this._todayObject.getMonth() + monthsToAdd, this._todayObject.getDate() + daysToAdd);
-
-		return this;
-	};
-
-	/**
-	 * Returns the date as a string formatted based on date format setting
-	 * @return {String}
-	 */
-	ODate.prototype.toString = function() {
-		return this.formatDate(this._dateObject, this.dateFormat);
-	};
-
-	/**
-	 * Returns the date
-	 * @return {Number}
-	 */
-	ODate.prototype.getDate = function() {
-		return this._dateObject.getDate();
-	};
-
-	/**
-	 * Returns the month (0-11)
-	 * @return {Number}
-	 */
-	ODate.prototype.getMonth = function() {
-		return this._dateObject.getMonth();
-	};
-
-	/**
-	 * Returns the full year (4 digits)
-	 * @return {Number}
-	 */
-	ODate.prototype.getFullYear = function() {
-		return this._dateObject.getFullYear();
+		return this._formatDate(new Date(this._dateObject.getFullYear() + yearsToAdd, this._dateObject.getMonth() + monthsToAdd, this._dateObject.getDate() + daysToAdd));
 	};
 
 	/**
@@ -95,35 +71,34 @@ var ODate;
 
 	/**
 	 * Returns a formatted date given a date object and the desired format
-	 * @param  {Date} dateObject A date object
-	 * @param  {String} format     The format we want. Check the supported expressions below
+	 * @param  {Date} date A date object
 	 * @return {String}            The formatted date
 	 */
-	ODate.prototype.formatDate = function(dateObject, format) {
-		var expressions = ["dddd", "ddd", "dd", "d", "mmmm", "mmm", "mm", "m", "yyyy", "yy"];
-		var replacements = [
-				this.regional[this.locale].dayNames[dateObject.getDay()], 
-				this.regional[this.locale].dayNamesShort[dateObject.getDay()], 
-				this._addZero(dateObject.getDate()), 
-				dateObject.getDate(), 
-				this.regional[this.locale].monthNames[dateObject.getMonth() + 1], 
-				this.regional[this.locale].monthNamesShort[dateObject.getMonth() + 1],
-				this._addZero(dateObject.getMonth() + 1), 
-				dateObject.getMonth(), 
-				dateObject.getFullYear(),
-				dateObject.getFullYear().toString().substring(2, 4)
-			];
+	ODate.prototype._formatDate = function(date) {
 
-		var result = format;
-
-		for (var i = 0, length = expressions.length; i < length; i++) {
-			var regEx = new RegExp(expressions[i]);
-			if (regEx.test(format)) {
-				result = result.replace(expressions[i], replacements[i]);
-				format = format.replace(expressions[i], "");
-			}
-
+		var expressions = {
+			"D": this.regional[this.locale].dayNames[date.getDay()],
+			"l": this.regional[this.locale].dayNamesShort[date.getDay()],
+			"d": this._addZero(date.getDate()),
+			"j": date.getDate(),
+			"F": this.regional[this.locale].monthNames[date.getMonth()],
+			"M": this.regional[this.locale].monthNamesShort[date.getMonth() + 1],
+			"m": this._addZero(date.getMonth() + 1),
+			"n": date.getMonth() + 1,
+			"Y": date.getFullYear(),
+			"y": date.getFullYear().toString().substring(2, 4)
 		}
+
+		var result = this.dateFormat;
+		var format = this.dateFormat;
+
+		for (var property in expressions) {
+			var regEx = new RegExp(property + "[\b]?", "g");
+			if (regEx.test(format)) {
+				result = result.replace(property, expressions[property]);
+				format = format.replace(property, "");
+			}
+		};
 
 		return result;
 	};
